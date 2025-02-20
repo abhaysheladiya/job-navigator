ee3e51255bd5edb408602e4d0622325a6193b965
abhaysheladiya <75667369+abhaysheladiya@users.noreply.github.com>
2025-02-20 19:10:05 +0530

    bycript the credentials.
    bycript the password.

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 const express= require("express");
 const mongoose = require('mongoose')
 const userModel =require('./models/users.js')
+const applicantModel =require('./models/applicant.js')
 const cors = require('cors')
+const bcrypt = require('bcrypt')
 
 const app = express();
 app.use(express.json())
@@ -10,16 +12,19 @@ app.use(cors())
 
 mongoose.connect("mongodb://127.0.0.1:27017/jobNavigator");
 
-app.post('/login',(req,res)=>{
+app.post("/login",(req, res)=>{
     const {email, password} = req.body;
-    userModel.findOne({email: email})
-    .then(user => {
+    userModel.findOne({ email: email })
+    .then((user) => {
         if(user) {
-           if(user.password === password ){
-            res.json("Success")
-           } else{
-            res.json("the password is incorrect")
-           }
+            bcrypt.compare(password, user.password, (err, response) =>{
+                if(err){ 
+                    return res.json("the password is incorrect") 
+                }
+                if(response){
+                    res.json("Success")
+                }
+            })
         }else{
             res.json("No record existed")
         }
@@ -28,11 +33,22 @@ app.post('/login',(req,res)=>{
 
 
 app.post('/register',(req,res) => {
-    userModel.create(req.body)
+    const {name, email, password} = req.body;
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        userModel.create({name, email, password : hash})
+        .then(users => res.json(job-navigator))
+        .catch(err => res.json(err))
+    }).catch(err => console.log(err.message))
+    
+})
+
+
+app.post('/applyjob',(req,res) => {
+    applicantModel.create(req.body)
     .then(users => res.json(job-navigator))
     .catch(err => res.json(err))
 })
-
 
 app.listen(5000, ()=>{
     console.log("app is running");
