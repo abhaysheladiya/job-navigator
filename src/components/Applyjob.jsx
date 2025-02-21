3ea8436b620d19207ddf29806e96c13e38c79726
abhaysheladiya <75667369+abhaysheladiya@users.noreply.github.com>
2025-02-21 22:22:25 +0530

    Multer implementation on frontend.

--- src/components/Applyjob.jsx
+++ src/components/Applyjob.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 function Applyjob() {
+
+  const [name, setName]= useState()
+  const [email, setEmail]= useState()
+  const [phone, setPhone]= useState()
+  const [coverlatter, setCoverlatter]= useState()
+  const [position, setPosition]= useState()
+  const navigate = useNavigate();
+  
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/applyjob", {
+        name,
+        email,
+        phone,
+        coverlatter,
+        position,
+      })
+      .then((result) => {
+        console.log(result);
+        // Navigate to the home page on successful submission
+        navigate("/home");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <div className="container">
         <h2>Job Application Form</h2>
-        <form>
-          <div className="mb-3">
+        <form className="p-4 p-md-5 border-0 rounded-3 shadow-sm bg-#f8f9fa"
+        onSubmit={handleSubmit} action="/upload" method="post" encType="multipart/form-data">
+          <div className="mb-2">
             <label htmlFor="fullName" className="form-label">
               Full Name
             </label>
@@ -16,9 +45,10 @@ function Applyjob() {
               id="fullName"
               placeholder="Enter your full name"
               required=""
+              onChange={(e)=> setName(e.target.value)}
             />
           </div>
-          <div className="mb-3">
+          <div className="mb-2">
             <label htmlFor="email" className="form-label">
               Email
             </label>
@@ -28,9 +58,10 @@ function Applyjob() {
               id="email"
               placeholder="Enter your email"
               required=""
+              onChange={(e)=> setEmail(e.target.value)}
             />
           </div>
-          <div className="mb-3">
+          <div className="mb-2">
             <label htmlFor="phone" className="form-label">
               Phone Number
             </label>
@@ -40,9 +71,10 @@ function Applyjob() {
               id="phone"
               placeholder="Enter your phone number"
               required=""
+              onChange={(e)=> setPhone(e.target.value)}
             />
           </div>
-          <div className="mb-3">
+          <div className="mb-2">
             <label htmlFor="resume" className="form-label">
               Upload Resume
             </label>
@@ -51,9 +83,10 @@ function Applyjob() {
               className="form-control"
               id="resume"
               required=""
+              name="resume"
             />
           </div>
-          <div className="mb-3">
+          <div className="mb-2">
             <label htmlFor="coverLetter" className="form-label">
               Cover Letter
             </label>
@@ -63,21 +96,23 @@ function Applyjob() {
               rows={4}
               placeholder="Write your cover letter here"
               defaultValue={""}
+              onChange={(e)=> setCoverlatter(e.target.value)}
             />
           </div>
-          <div className="mb-3">
+          <div className="mb-2">
             <label htmlFor="position" className="form-label">
               Position Applied For
             </label>
-            <select className="form-select" id="position" required="">
+            <select className="form-select" id="position" required=""
+            onChange={(e)=> setPosition(e.target.value)}>
               <option value="" selected="" disabled="">
                 Select a position
               </option>
               <option value="developer">Software Developer</option>
-              <option value="designer">UI/UX Designer</option>
-              <option value="manager">Project Manager</option>
-              <option value="manager">Cyber Security</option>
-              <option value="manager">BlockChain</option>
+              <option value="UI/UX">UI/UX Designer</option>
+              <option value="Project manager">Project Manager</option>
+              <option value="Cyber Security">Cyber Security</option>
+              <option value="Blockchain">BlockChain</option>
             </select>
           </div>
           <button type="submit" className="btn btn-primary">
