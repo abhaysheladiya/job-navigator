16b7c25d9b8056b5dcb4984335f7d529b9aa5e0e
abhaysheladiya <75667369+abhaysheladiya@users.noreply.github.com>
2025-02-25 21:32:29 +0530

    Forgot-password APIs implementation

--- Backend/index.js
+++ Backend/index.js
@@ -4,8 +4,12 @@ const userModel =require('./models/users.js')
 const applicantModel =require('./models/applicant.js')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
+const jwt= require('jsonwebtoken')
+const nodemailer= require('nodemailer')
+
 const multer =require("multer");
 const path = require("path");
+const { resolve } = require("path/posix");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -63,7 +67,7 @@ app.post('/applyjob',(req,res) => {
     .catch(err => res.json(err))
 })
 
-app.post("/upload", upload.single("resume"), (req, res)=>{
+app.post("/upload", upload.single("resumeupload"), (req, res)=>{
     console.log(req. body);
     console.log(req.file);
 
@@ -73,3 +77,60 @@ app.post("/upload", upload.single("resume"), (req, res)=>{
 app.listen(5000, ()=>{
     console.log("app is running");
 });
+
+
+
+
+// forgot-password APIs
+app.post('/forgot-password', (req, res)=>{
+    const {email} = req.body;
+    userModel.findOne({ email: email })
+    .then(user => {
+        if(!user){
+            return res.send({Status: "User not existed"})
+        }
+        const token= jwt.sign({id: user._id}, "jwt_secret_key", {expiresIn: "1d"})
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'youremail@gmail.com',
+              pass: 'yourpassword'
+            }
+          });
+          
+          var mailOptions = {
+            from: 'youremail@gmail.com',
+            to: 'myfriend@yahoo.com',
+            subject: 'Reset your password',
+            text: `http://localhost:5173/reset-password/${user._id}/${token}`
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              return res.send({Status: "Success"})
+            }
+          });
+          
+    })
+})
+
+app.post('/reset-password/:id/:token',(req,res)=>{
+    const {id, token} =req.params
+    const {password} = req.body
+
+    jwt.verify(token, "jwt_secret_key",(err, decoded)=>{
+        if(err){
+            return res.json({Status: "Error with token"})
+        }else{
+            bcrypt.hash(password, 10)
+            .then(hash => {
+                userModel.findByIdAndUpdate({_id: id}, {password: hash})
+                .then(u => res.send({Status: "Success"}))
+                .catch(err => res.send({Status: err}))
+            })
+            .catch(err=> res.send({Status: err}))
+        }
+    })
+})
